5867e6521271b93b7a7c678e96f13613c95f622a
bluesea <jangwj98@gmail.com>
2026-05-24 14:45:31 +0900

    fix: louder beep, improve camera constraints

--- components/ui/BarcodeCamera.tsx
+++ components/ui/BarcodeCamera.tsx
@@ -14,12 +14,12 @@ const playBeep = () => {
     const gainNode = ctx.createGain();
     oscillator.connect(gainNode);
     gainNode.connect(ctx.destination);
-    oscillator.frequency.value = 1800;
+    oscillator.frequency.value = 2500;
     oscillator.type = 'sine';
-    gainNode.gain.setValueAtTime(0.3, ctx.currentTime);
-    gainNode.gain.exponentialRampToValueAtTime(0.001, ctx.currentTime + 0.15);
+    gainNode.gain.setValueAtTime(0.8, ctx.currentTime);
+    gainNode.gain.exponentialRampToValueAtTime(0.001, ctx.currentTime + 0.2);
     oscillator.start(ctx.currentTime);
-    oscillator.stop(ctx.currentTime + 0.15);
+    oscillator.stop(ctx.currentTime + 0.2);
   } catch {}
 };
 
@@ -37,6 +37,9 @@ export default function BarcodeCamera({ onScan, onClose }: BarcodeCameraProps) {
           width: { ideal: 1920 },
           height: { ideal: 1080 },
           focusMode: 'continuous',
+          zoom: { ideal: 1.5 },
+          exposureMode: 'continuous',
+          whiteBalanceMode: 'continuous',
         } as MediaTrackConstraints,
       },
       videoRef.current!,
